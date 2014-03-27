b6049e64c89751802e4117c2948a5f9cd2c5ece2
Mariusz Nowak <mariusz@medikoo.com>
2014-03-27 15:12:55 +0100

    Toggle as well url search token

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 'use strict';
 
-var parse   = require('querystring/decode')
-  , loadCss = require('webmake/lib/browser/load-css');
+var parse     = require('querystring/decode')
+  , stringify = require('querystring/encode')
+  , loadCss   = require('webmake/lib/browser/load-css')
+
+  , resolveQuery;
 
 require('./style');
+
+resolveQuery = function (token) {
+	var value;
+	if (!location.search) return null;
+	value = parse(location.search.slice(1))[token];
+	if (value == null) return null;
+	if (!value) return true;
+	return Boolean(Number(value));
+};
 
 module.exports = function (deck/*, options*/) {
 	var options = Object(arguments[1])
 	  , visible = Boolean(options.visible)
 	  , key = options.key || 0x4e // 'n' key
-	  , query = location.search ? parse(location.search.slice(1)) : {}
-	  , slideWidth = options.slideWidth;
+	  , slideWidth = options.slideWidth
+	  , queryToken = 'notes', update, current, resolvedQuery;
 
-	if (query.notes != null) {
-		visible = !query.notes || Boolean(Number(query.notes));
+	if (options.queryToken === false) {
+		queryToken = null;
+	} else if (options.queryToken && (options.queryToken !== true)) {
+		queryToken = (options.queryToken);
+	}
+
+	if (queryToken) {
+		resolvedQuery = resolveQuery(queryToken);
+		if (resolvedQuery != null) visible = resolvedQuery;
+		window.addEventListener('popstate', function () {
+			update(Boolean(resolveQuery(queryToken)));
+		});
 	}
 	if (!isNaN(slideWidth) && (slideWidth > 0)) {
 		loadCss('body.notes .bespoke-slide {' +
@@ -26,14 +48,36 @@ module.exports = function (deck/*, options*/) {
 			'}');
 	}
 
+	update = function (visible) {
+		var query, search, url;
+		if (current === visible) return;
+		current = visible;
+		if (visible) {
+			document.body.classList.add('notes');
+		} else {
+			document.body.classList.remove('notes');
+		}
+		if (!queryToken) return;
+		url = location.pathname;
+		if (location.search) query = parse(location.search.slice(1));
+		else query = {};
+		if (visible) query[queryToken] = null;
+		else delete query[queryToken];
+		search = stringify(query);
+		if (search) {
+			url += '?' + search.replace(/(?:=&)/g, '&').replace(/\=$/, '');
+		}
+		if (location.hash) url += location.hash;
+		history.pushState({}, '', url);
+	};
+
 	document.addEventListener('keydown', function (e) {
 		if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
 		if (e.which !== key) return;
 		e.preventDefault();
 
-		document.body.classList[visible ? 'remove' : 'add']('notes');
-		visible = !visible;
+		update(!current);
 	}, false);
 
-	if (visible) document.body.classList.add('notes');
+	update(visible);
 };

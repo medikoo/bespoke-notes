e6c4246a35be007b8eadf5dcd3a9a680ccc6ab01
Mariusz Nowak <mariusz@medikoo.com>
2014-09-08 16:13:46 +0200

    Update for Bespoke 1.0

--- index.js
+++ index.js
@@ -25,64 +25,65 @@ invokeResize = function () {
 	window.dispatchEvent(event);
 };
 
-module.exports = function (deck/*, options*/) {
+module.exports = function (/*options*/) {
 	var options = Object(arguments[1])
 	  , visible = Boolean(options.visible)
 	  , key = options.key || 0x4e // 'n' key
 	  , slideWidth = options.slideWidth
-	  , queryToken = 'notes', update, current, resolvedQuery;
+	  , queryToken = 'notes';
 
-	if (options.queryToken === false) {
-		queryToken = null;
-	} else if (options.queryToken && (options.queryToken !== true)) {
-		queryToken = (options.queryToken);
-	}
+	if (options.queryToken === false) queryToken = null;
+	else if (options.queryToken && (options.queryToken !== true)) queryToken = options.queryToken;
 
-	if (queryToken) {
-		resolvedQuery = resolveQuery(queryToken);
-		if (resolvedQuery != null) visible = resolvedQuery;
-		window.addEventListener('popstate', function () {
-			update(Boolean(resolveQuery(queryToken)));
-		});
-	}
-	if (!isNaN(slideWidth) && (slideWidth > 0)) {
-		loadCss('body.notes .bespoke-slide {' +
-			'width: ' + (slideWidth * 2) + 'px;' +
-			'margin-left: -' + slideWidth + 'px;' +
-			'}' +
-			'body.notes .bespoke-slide > div.content,' +
-			'body.notes .bespoke-slide > aside {' +
-			'width: ' + (slideWidth - 40) + 'px;' +
-			'}');
-	}
+	return function (deck) {
+		var update, current, resolvedQuery;
 
-	update = function (visible) {
-		var query, search, url;
-		if (current === visible) return;
-		current = visible;
-		if (visible) document.body.classList.add('notes');
-		else document.body.classList.remove('notes');
-		if (!queryToken) return;
-		url = location.pathname;
-		if (location.search) query = parse(location.search.slice(1));
-		else query = {};
-		if (visible) query[queryToken] = null;
-		else delete query[queryToken];
-		search = stringify(query);
-		if (search) url += '?' + search.replace(/(?:=&)/g, '&').replace(/\=$/, '');
-		if (location.hash) url += location.hash;
-		history.pushState({}, '', url);
-		invokeResize();
+		if (queryToken) {
+			resolvedQuery = resolveQuery(queryToken);
+			if (resolvedQuery != null) visible = resolvedQuery;
+			window.addEventListener('popstate', function () {
+				update(Boolean(resolveQuery(queryToken)));
+			});
+		}
+		if (!isNaN(slideWidth) && (slideWidth > 0)) {
+			loadCss('body.notes .bespoke-slide {' +
+				'width: ' + (slideWidth * 2) + 'px;' +
+				'margin-left: -' + slideWidth + 'px;' +
+				'}' +
+				'body.notes .bespoke-slide > div.content,' +
+				'body.notes .bespoke-slide > aside {' +
+				'width: ' + (slideWidth - 40) + 'px;' +
+				'}');
+		}
+
+		update = function (visible) {
+			var query, search, url;
+			if (current === visible) return;
+			current = visible;
+			if (visible) document.body.classList.add('notes');
+			else document.body.classList.remove('notes');
+			if (!queryToken) return;
+			url = location.pathname;
+			if (location.search) query = parse(location.search.slice(1));
+			else query = {};
+			if (visible) query[queryToken] = null;
+			else delete query[queryToken];
+			search = stringify(query);
+			if (search) url += '?' + search.replace(/(?:=&)/g, '&').replace(/\=$/, '');
+			if (location.hash) url += location.hash;
+			history.pushState({}, '', url);
+			invokeResize();
+		};
+
+		document.addEventListener('keydown', function (e) {
+			if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
+			if (e.which !== key) return;
+			if (ignoredContexts[e.srcElement.nodeName.toLowerCase()]) return;
+			e.preventDefault();
+
+			update(!current);
+		}, false);
+
+		update(visible);
 	};
-
-	document.addEventListener('keydown', function (e) {
-		if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
-		if (e.which !== key) return;
-		if (ignoredContexts[e.srcElement.nodeName.toLowerCase()]) return;
-		e.preventDefault();
-
-		update(!current);
-	}, false);
-
-	update(visible);
 };
